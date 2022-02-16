37fe691e86d6f6ec89fa7ae432bf528f46f7be31
Kaleb Glodowski <80715870+KalebGlodowski@users.noreply.github.com>
2022-02-15 21:36:42 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+//array
+var warriors = [
+    {
+        name:"Bob",
+        damage:2,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Jerry",
+        damage:1,
+        health:12,
+        warrior:true
+    },
+    {
+        name:"Mavis",
+        damage:2,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Morty",
+        damage:4,
+        health:10,
+        warrior:true
+    },
+    {
+        name:"Shorty",
+        damage:10,
+        health:3,
+        warrior:false
+    },
+    {
+        name:"Porty",
+        damage:1,
+        health:14,
+        warrior:true
+    },{
+        name:"Perry",
+        damage:2,
+        health:9,
+        warrior:true
+    },
+    {
+        name:"Larry",
+        damage:2,
+        health:17,
+        warrior:false
+    }
+]
+
+//for loop to render elements
+for (let i=0; i<warriors.length; i++){
+
+    if (warriors[i].health >= 10 && warriors[i].damage >= 2 && warriors[i].warrior === true) {
+        let ele = document.createElement("div");
+        let nameEle = document.createElement("h1");
+        let healthEle = document.createElement("h3");
+        let damageEle = document.createElement("h3");
+        let warriorEle = document.createElement("h3");
+        nameEle.innerHTML=warriors[i].name;
+        healthEle.innerHTML=warriors[i].health;
+        damageEle.innerHTML=warriors[i].damage;
+        warriorEle.innerHTML=warriors[i].warrior;
+        ele.appendChild(nameEle);
+        ele.appendChild(healthEle);
+        ele.appendChild(damageEle);
+        ele.appendChild(warriorEle);
+
+        for (let j=0; j<warriors[i].name.length; j++) {
+            console.log(warriors[i].name[j]);
+            if (warriors[i].name[j] === "a" || warriors[i].name[j] === "A") {
+                console.log("Found an A!");
+            }
+        }
+        document.body.appendChild(ele);
+    }
+}
+
+
+
+// Only display characters that have
+    // 10 or more health (number)
+    // deal 2 or more damage (number)
+    // and are a warrior (true/false)
+    // If the character has the letter "A" in it, make the text red
